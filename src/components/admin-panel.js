a7de941caa67ae57f0b1e3bcd090f3fb28f972cc
Muhammet <muhammet075@icloud.com>
2025-08-22 02:33:54 +0200

    working on admin panel

--- src/components/admin-panel.js
+++ src/components/admin-panel.js
@@ -1,15 +1,48 @@
-import Image from "next/image";
-import Link from "next/link";
+import { useState } from "react";
 import styles from "@/styles/admin-panel.module.css";
 
-
 function Adminpanel() {
+  const [label, setLabel] = useState("");
+  const [title, setTitle] = useState("");
+  const [optionalBody, setOptionalBody] = useState("");
+  const [optionalButton, setOptionalButton] = useState("");
 
-    
+  function saveData() {
+    alert("Opgeslagen");
+    console.log({
+      label,
+      title,
+      optionalBody,
+      optionalButton,
+    });
+  }
+
   return (
     <div className={styles.adminpanel}>
       <div>
-        <p>Admin panel</p>
+
+        <section>
+        <p>Label</p>
+        <input id="labelinput" value={label} onChange={(e) => setLabel(e.target.value)} />
+        </section>
+
+        <section>
+        <p>Title</p>
+        <input id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        </section>
+
+        <section>
+        <p>Optional body</p>
+        <input id="optionalbody" value={optionalBody} onChange={(e) => setOptionalBody(e.target.value)} />
+        </section>
+
+        <section>
+        <p>Optional button</p>
+        <input id="optionalbutton" value={optionalButton} onChange={(e) => setOptionalButton(e.target.value)} />
+        </section>
+
+        <button onClick={saveData}>Opslaan</button>
+        
       </div>
     </div>
   );
--- src/components/cta-section.js
+++ src/components/cta-section.js
@@ -1,8 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "@/styles/cta-section.module.css";
+import Adminpanel, { ctaData } from "@/components/admin-panel";
 
 function CtaSection() {
+
+  
+
+  
+
   return (
     <div className={styles.ctasection}>
       <div>
