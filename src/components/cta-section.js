5189806f24ce1042f6c3898c119fde5d9551a507
Muhammet <muhammet075@icloud.com>
2025-08-23 03:33:24 +0200

    working on displaying data

--- src/components/cta-section.js
+++ src/components/cta-section.js
@@ -1,20 +1,25 @@
-import { useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { doc, getDoc } from "firebase/firestore";
 import { db } from "../components/firebase";
-
 import styles from "@/styles/cta-section.module.css";
+import arrowRight from "@/assets/icons/arrow-right.svg";
 
 export default function CtaSection() {
+  const [ctaData, setCtaData] = useState(null); // enkel 1 document
 
   useEffect(() => {
     const fetchCtaData = async () => {
       try {
-        const querySnapshot = await getDocs(collection(db, "ctaData"));
-        const dataArray = [];
-        querySnapshot.forEach(doc => {
-          dataArray.push({ id: doc.id, ...doc.data() });
-        });
-        console.log(dataArray);
+        const docRef = doc(db, "ctaData", "mainCTA"); // precies hetzelfde document als in Admin panel
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          setCtaData(docSnap.data()); // data in state zetten
+        } else {
+          console.log("Geen data gevonden!");
+        }
       } catch (error) {
         console.error(error);
       }
@@ -23,11 +28,38 @@ export default function CtaSection() {
     fetchCtaData();
   }, []);
 
+  if (!ctaData) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className={styles.ctasection}>
       <div>
-        <p>ctaSection</p>
+        <p>{ctaData.label}</p>
+        <h2>{ctaData.title}</h2>
+        {ctaData.optionalBody && <p>{ctaData.optionalBody}</p>}
+        {ctaData.optionalButton && <button>{ctaData.optionalButton}</button>}
       </div>
+
+
+        {ctaData.cards && ctaData.cards.length > 0 && (
+          <div className={styles.cards}>
+            {ctaData.cards.map((card, index) => (
+              <section key={index} className={styles.card}>
+                <div>
+                  <div>
+                    <div>
+                      <h3>{card.title}</h3>
+                      <p>{card.description}</p>
+                    </div>
+                    {card.link && <Link target="_blank" href={card.link}><Image src={arrowRight} alt="Right arrow icon"/></Link>}
+                  </div>
+                </div>
+              </section>
+            ))}
+          </div>
+        )}
+
     </div>
   );
 }
